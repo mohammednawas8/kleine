357ba41d56f58d2fff0a39ec4b84e6d67f6d0dee
Mohammad nawas <mohammednawas8@gmail.com>
2022-02-10 19:12:49 +0200

    Regiannimation

--- app/src/main/java/com/example/kleine/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/kleine/fragments/RegisterFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import br.com.simplepass.loadingbutton.customViews.CircularProgressButton
 import com.example.kleine.R
 import com.example.kleine.databinding.FragmentRegisterBinding
 
@@ -18,6 +19,21 @@ class RegisterFragment : Fragment() {
     ): View? {
         binding = FragmentRegisterBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnRegister = view.findViewById<CircularProgressButton>(R.id.btn_login)
+
+        btnRegister.setOnClickListener {
+            btnRegister.spinningBarColor = resources.getColor(R.color.g_white)
+            btnRegister.spinningBarWidth = resources.getDimension(R.dimen._20sdp)
+            btnRegister.startAnimation()
+
+            //btnRegister.revertAnimation()
+        }
+
     }
 
 }
