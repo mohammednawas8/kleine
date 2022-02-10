a67266b011d1f78b0fed561f7de0c18d0916dd28
Mohammad nawas <mohammednawas8@gmail.com>
2022-02-10 19:18:14 +0200

    buttons annimation

--- app/src/main/java/com/example/kleine/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/kleine/fragments/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import br.com.simplepass.loadingbutton.customViews.CircularProgressButton
 import com.example.kleine.R
 import com.example.kleine.databinding.FragmentLoginBinding
 
@@ -18,6 +19,21 @@ class LoginFragment : Fragment() {
     ): View? {
         binding = FragmentLoginBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val btnLogin = view.findViewById<CircularProgressButton>(R.id.btn_login)
+        btnLogin.setOnClickListener {
+            btnLogin.apply {
+                spinningBarColor = resources.getColor(R.color.g_white)
+                spinningBarWidth = resources.getDimension(R.dimen._20sdp)
+            }
+
+            btnLogin.startAnimation()
+        }
     }
 
 
