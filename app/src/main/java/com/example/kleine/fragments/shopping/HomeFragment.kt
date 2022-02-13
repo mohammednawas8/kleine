e38adc37ab6287aa95a31506b216a076480861a0
mohammad <mohammednawas8@gmail.com>
2022-02-13 14:53:02 +0200

    home fragment design

---
+++ app/src/main/java/com/example/kleine/fragments/shopping/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.kleine.fragments.shopping
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kleine.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding:FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+
+}
