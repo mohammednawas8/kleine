72ce74a0e5006652815afe0ad4cf0f89ff6a3670
Mohammad nawas <mohammednawas8@gmail.com>
2022-02-16 17:34:53 +0200

    AdADw

---
+++ app/src/main/java/com/example/kleine/adapters/viewpager/ViewPager2Images.kt
@@ -0,0 +1,41 @@
+package com.example.kleine.adapters.viewpager
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.kleine.databinding.ViewpagerImageItemBinding
+
+class ViewPager2Images : RecyclerView.Adapter<ViewPager2Images.ViewPager2ImagesViewHolder>() {
+    inner class ViewPager2ImagesViewHolder(val binding:ViewpagerImageItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, diffCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPager2ImagesViewHolder {
+        return ViewPager2ImagesViewHolder(
+            ViewpagerImageItemBinding.inflate(
+                LayoutInflater.from(parent.context)
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewPager2ImagesViewHolder, position: Int) {
+        Glide.with(holder.itemView).load(differ.currentList[position]).into(holder.binding.imgsProduct)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+}
--- app/src/main/java/com/example/kleine/fragments/shopping/ProductPreviewFragment.kt
+++ app/src/main/java/com/example/kleine/fragments/shopping/ProductPreviewFragment.kt
@@ -5,12 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kleine.R
+import com.example.kleine.adapters.recyclerview.ColorsAndSizesAdapter
+import com.example.kleine.adapters.viewpager.ViewPager2Images
 import com.example.kleine.databinding.FragmentProductPreviewBinding
+import com.example.kleine.util.Constants.Companion.COLORS_TYPE
+import com.example.kleine.util.Constants.Companion.SIZES_TYPE
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ProductPreviewFragment : Fragment() {
-    private lateinit var binding:FragmentProductPreviewBinding
+    private lateinit var binding: FragmentProductPreviewBinding
+    private lateinit var colorsAdapter: ColorsAndSizesAdapter
+    private lateinit var sizesAdapter: ColorsAndSizesAdapter
+    private lateinit var viewPagerAdapter:ViewPager2Images
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        colorsAdapter = ColorsAndSizesAdapter(COLORS_TYPE)
+        sizesAdapter = ColorsAndSizesAdapter(SIZES_TYPE)
+        viewPagerAdapter = ViewPager2Images()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,12 +39,34 @@ class ProductPreviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val bottomNavigation = requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val bottomNavigation =
+            requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigation.visibility = View.GONE
 
+        setupViewpager()
+        setupColorsRecyclerview()
+        setupSizesRecyclerview()
 
-        
 
+
+    }
+
+    private fun setupSizesRecyclerview() {
+        binding.rvColors.apply {
+            adapter = sizesAdapter
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        }
+    }
+
+    private fun setupColorsRecyclerview() {
+        binding.rvColors.apply {
+            adapter = colorsAdapter
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        }
+    }
+
+    private fun setupViewpager() {
+        binding.viewpager2Images.adapter = viewPagerAdapter
     }
 
 }
