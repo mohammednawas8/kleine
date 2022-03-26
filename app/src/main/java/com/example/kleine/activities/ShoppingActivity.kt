dbbfc3e18171297e8e37fd96f8f5447504b5b29c
mohammad <mohammednawas8@gmail.com>
2022-03-26 14:57:10 +0300

    Store activity

--- app/src/main/java/com/example/kleine/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/kleine/activities/ShoppingActivity.kt
@@ -34,7 +34,7 @@ class ShoppingActivity : AppCompatActivity() {
 
 
         cartViewModel = CartViewModel()
-        supportActionBar!!.hide()
+//        supportActionBar!!.hide()
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navController = Navigation.findNavController(this, R.id.host_fragment)
