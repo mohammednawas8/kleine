1ffe7e9d6ebf5e2dd1888be68e011d56f92bff14
mohammad <mohammednawas8@gmail.com>
2022-03-29 23:17:09 +0300

    Create storeAppFragments

--- app/src/main/java/com/example/kleine/activities/StoreActivity.kt
+++ app/src/main/java/com/example/kleine/activities/StoreActivity.kt
@@ -6,8 +6,12 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.forEach
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentTransaction
 import com.example.kleine.R
 import com.example.kleine.databinding.ActivityStoreBinding
+import com.example.kleine.fragments.BlankFragment
+import com.example.kleine.fragments.categories.FurnitureFragment
+import com.example.kleine.fragments.categories.TableFragment
 
 class StoreActivity : AppCompatActivity() {
 
@@ -22,11 +26,17 @@ class StoreActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setSupportActionBar(binding.topAppBar)
         toggle = ActionBarDrawerToggle(this,binding.drawerLayout,R.string.open_menu,R.string.close_menu)
-        binding.navigationView.setCheckedItem(0)
 
+        val fragmentManger = supportFragmentManager
+        val fragmentTransaction = fragmentManger.beginTransaction()
+//        fragmentTransaction.replace(R.id.fragment_host,BlankFragment())
+        fragmentTransaction.commit()
+
+        binding.navigationView.menu.getItem(0).isChecked = true
 
         binding.navigationView.setNavigationItemSelectedListener { menuItem ->
             menuItem.isChecked = true
+
             binding.navigationView.menu.forEach { unselectedItems ->
                 if(unselectedItems != menuItem)
                     unselectedItems.isChecked = false
@@ -35,12 +45,20 @@ class StoreActivity : AppCompatActivity() {
             true
         }
 
+
+
     }
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)) {
             return true
+        }
+
+        when(item.itemId){
+            R.id.home -> {
+
+            }
         }
         return super.onOptionsItemSelected(item)
     }
